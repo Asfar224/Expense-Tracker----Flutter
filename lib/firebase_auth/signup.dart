c00d60ad1e0e426ee52c9174b1ac9ba394260226
Asfar <asfar.abid.chaudhay@gmail.com>
2024-12-23 01:29:49 +0500

    scrollable veiw in signup page

--- lib/firebase_auth/signup.dart
+++ lib/firebase_auth/signup.dart
@@ -12,7 +12,6 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  // Controllers to retrieve the values from the input fields
   late final TextEditingController _nameController;
   late final TextEditingController _emailController;
   late final TextEditingController _passwordController;
@@ -35,13 +34,10 @@ class _SignupScreenState extends State<SignupScreen> {
 
   Future<User?> signUpWithGoogle() async {
     final GoogleSignIn googleSignIn = await GoogleSignIn();
-
     final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-
     if (googleUser == null) {
       return null;
     }
-
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
 
@@ -59,29 +55,30 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20.0, top: 25.0, right: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 65.0),
-            Text(
-              "Create an account",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.only(left: 20.0, top: 25.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 65.0),
+              Text(
+                "Create an account",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 20.0), // Space between text and input fields
+              SizedBox(height: 20.0),
 
-            // Name input field
-            Text('Name',
-                style: TextStyle(fontSize: 16.0)), // Label above input field
-            SizedBox(height: 8.0), // Space between label and input field
-            TextFormField(
-              controller: _nameController,
-              decoration: InputDecoration(
+              // Name input field
+              Text('Name', style: TextStyle(fontSize: 16.0)),
+              SizedBox(height: 8.0),
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
                   hintText: 'John Doe',
                   hintStyle: TextStyle(
                       color: const Color.fromARGB(255, 153, 153, 153)),
@@ -93,17 +90,18 @@ class _SignupScreenState extends State<SignupScreen> {
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
                     borderRadius: BorderRadius.circular(10.0),
-                  )),
-            ),
-            SizedBox(height: 18.0), // Space between input fields
+                  ),
+                ),
+              ),
+              SizedBox(height: 18.0),
 
-            // Email input field
-            Text('Email', style: TextStyle(fontSize: 16.0)),
-            SizedBox(height: 8.0),
-            TextFormField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
+              // Email input field
+              Text('Email', style: TextStyle(fontSize: 16.0)),
+              SizedBox(height: 8.0),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
                   hintText: 'hello@example.com',
                   hintStyle: TextStyle(
                       color: const Color.fromARGB(255, 153, 153, 153)),
@@ -115,20 +113,21 @@ class _SignupScreenState extends State<SignupScreen> {
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
                     borderRadius: BorderRadius.circular(10.0),
-                  )),
-            ),
-            SizedBox(height: 18.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 18.0),
 
-            // Password input field
-            Text(
-              "Password",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            SizedBox(height: 8.0),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
+              // Password input field
+              Text(
+                "Password",
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
                   hintStyle: TextStyle(
                       color: const Color.fromARGB(255, 153, 153, 153)),
                   enabledBorder: OutlineInputBorder(
@@ -139,163 +138,158 @@ class _SignupScreenState extends State<SignupScreen> {
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
                     borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 28.0),
+
+              Text("By continuing, you agree to our terms of service.",
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    wordSpacing: 3.0,
                   )),
-            ),
-            SizedBox(height: 28.0),
+              SizedBox(height: 16.0),
 
-            Text("By continuing, you agree to our terms of service. ",
-                style: TextStyle(
-                  color: Colors.grey[500],
-                  wordSpacing: 3.0,
-                )),
-            SizedBox(height: 16.0),
+              // Sign Up button
+              ElevatedButton(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Center(
+                      child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 78, 21, 144),
+                  minimumSize: Size(double.infinity, 10),
+                  textStyle: TextStyle(fontSize: 18.0),
+                ),
+                onPressed: () async {
+                  final email = _emailController.text;
+                  final password = _passwordController.text;
+                  final name = _nameController.text;
 
-            // Sign Up button
-            ElevatedButton(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: Center(
-                    child: Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.white),
-                )),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 78, 21, 144),
-                minimumSize:
-                    Size(double.infinity, 10), // Make button full width
-                textStyle: TextStyle(fontSize: 18.0),
-              ),
-              onPressed: () async {
-                final email = _emailController.text;
-                final password = _passwordController.text;
-                final name = _nameController.text;
+                  if (email.isNotEmpty &&
+                      password.isNotEmpty &&
+                      name.isNotEmpty) {
+                    if (password.length < 8) {
+                      ErrorOverlay.show(
+                          context, "Password must be 8 characters long");
+                    } else {
+                      try {
+                        final UserCredential userCredential = await FirebaseAuth
+                            .instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
 
-                if (email.isNotEmpty &&
-                    password.isNotEmpty &&
-                    name.isNotEmpty) {
-                  if (password.length < 8) {
-                    ErrorOverlay.show(
-                        context, "Password must be 8 characters long");
-                  } else {
-                    try {
-                      final UserCredential userCredential = await FirebaseAuth
-                          .instance
-                          .createUserWithEmailAndPassword(
-                              email: email, password: password);
+                        String uid = userCredential.user!.uid;
 
-                      String uid = userCredential.user!.uid;
+                        await FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(uid)
+                            .set({
+                          'name': name,
+                          'createdAt': FieldValue.serverTimestamp(),
+                        });
 
-                      await FirebaseFirestore.instance
-                          .collection('users')
-                          .doc(uid)
-                          .set({
-                        'name': name,
-                        'createdAt': FieldValue.serverTimestamp(),
-                      });
-
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              LandingPage(uid: uid), // Pass UID to LandingPage
-                        ),
-                      );
-                    } catch (exception) {
-                      ErrorOverlay.show(context, "Authentication Unsuccessful");
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LandingPage(uid: uid),
+                          ),
+                        );
+                      } catch (exception) {
+                        ErrorOverlay.show(
+                            context, "Authentication Unsuccessful");
+                      }
                     }
+                  } else {
+                    ErrorOverlay.show(context, "Fill out the required fields");
                   }
-                } else {
-                  ErrorOverlay.show(context, "Fill out the required feilds");
-                }
-              },
-            ),
-            SizedBox(height: 30.0),
+                },
+              ),
+              SizedBox(height: 30.0),
 
-            // Divider
-
-            const Row(
-              children: <Widget>[
-                Expanded(
-                  child: Divider(
-                    color: Color.fromARGB(255, 230, 221, 221), // Line color
-                    thickness: 1, // Line thickness
+              // Divider
+              const Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Divider(
+                      color: Color.fromARGB(255, 230, 221, 221),
+                      thickness: 1,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "or",
-                    style: TextStyle(color: Color.fromARGB(255, 202, 188, 188)),
-                  ), // Text in the middle
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Color.fromARGB(255, 230, 221, 221), // Line color
-                    thickness: 1, // Line thickness
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 30.0),
-            ElevatedButton(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Center(
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
                     child: Text(
-                  'Continue with google',
-                  style:
-                      TextStyle(color: const Color.fromARGB(255, 72, 70, 70)),
-                )),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 179, 176, 179),
-                minimumSize:
-                    Size(double.infinity, 10), // Make button full width
-                textStyle: TextStyle(fontSize: 18.0),
-              ),
-              onPressed: () async {
-                await signUpWithGoogle();
-              },
-            ),
-            SizedBox(
-              height: 18.0,
-            ),
-
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Already have an account?",
-                    style: TextStyle(color: Colors.grey),
+                      "or",
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 202, 188, 188)),
+                    ),
                   ),
-                  SizedBox(width: 5), // Add space between texts
-                  GestureDetector(
-                    onTap: () {
-                      // Navigate to the login page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              LoginScreen(), // Your login screen
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      "Login Here",
-                      style: TextStyle(
-                        color: const Color.fromARGB(
-                            255, 16, 122, 208), // Blue color for link
-                        fontWeight: FontWeight.bold,
-                      ),
+                  Expanded(
+                    child: Divider(
+                      color: Color.fromARGB(255, 230, 221, 221),
+                      thickness: 1,
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Center(
+                      child: Text(
+                    'Continue with Google',
+                    style:
+                        TextStyle(color: const Color.fromARGB(255, 72, 70, 70)),
+                  )),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 179, 176, 179),
+                  minimumSize: Size(double.infinity, 10),
+                  textStyle: TextStyle(fontSize: 18.0),
+                ),
+                onPressed: () async {
+                  await signUpWithGoogle();
+                },
+              ),
+              SizedBox(height: 18.0),
+
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(width: 5),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Login Here",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 16, 122, 208),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
